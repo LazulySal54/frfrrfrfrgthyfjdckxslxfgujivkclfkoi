212ab92afb58ad104b0725a00510d6ec2130f2e5
LazulySal54 <56200060+LazulySal54@users.noreply.github.com>
2020-06-14 13:38:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const { CommandoClient } = require('discord.js-commando');
+const { Structures } = require('discord.js');
+const path = require('path');
+const { prefix, token, discord_owner_id } = require('./config.json');
+
+Structures.extend('Guild', function(Guild) {
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null,
+        volume: 1
+      };
+      this.triviaData = {
+        isTriviaRunning: false,
+        wasTriviaEndCalled: false,
+        triviaQueue: [],
+        triviaScore: new Map()
+      };
+    }
+  }
+  return MusicGuild;
+});
+
+const client = new CommandoClient({
+  commandPrefix: prefix,
+  owner: discord_owner_id // value comes from config.json
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', '<:musique:719866420221181952> Commande Musique'],
+    ['guild', '<:staff:719866778473463888> Commandes Modération']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    eval: false,
+    prefix: false,
+    commandState: false
+  })
+  .registerCommandsIn(path.join(__dirname, 'commandes'));
+
+client.once('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity(`${prefix}help`, {
+    type: 'WATCHING',
+    url: 'https://github.com/qzebulon'
+  });
+});
+
+client.on('voiceStateUpdate', async (___, newState) => {
+  if (
+    newState.member.user.bot &&
+    !newState.channelID &&
+    newState.guild.musicData.songDispatcher &&
+    newState.member.user.id == client.user.id
+  ) {
+    newState.guild.musicData.queue.length = 0;
+    newState.guild.musicData.songDispatcher.end();
+  }
+});
+
+client.login(token);
